ec45ba6d6e3da2d7411b4a4007b61a32852d2242
alfredo215 <54558738+alfredo215@users.noreply.github.com>
2020-06-11 10:43:38 -0600

    datos en tablas

--- ProyectoSpring/src/main/java/com/Spring/Controlador.java
+++ ProyectoSpring/src/main/java/com/Spring/Controlador.java
@@ -1,9 +1,13 @@
 package com.Spring;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.Spring.model.Persona;
 
 //@RestController
 @Controller
@@ -11,10 +15,27 @@ public class Controlador {
 	
 	@GetMapping("/")
 	public String Inicio(Model modelo){
+		
+		Persona person = new Persona();
+		person.setNombre("Alfredo");
+		person.setApellido("Moran");
+		person.setTelefono("75853-328");
+		person.setEmail("calfredomoran@gmail.com");
+		
+		Persona person2 = new Persona();
+		person2.setNombre("Pedro");
+		person2.setApellido("Perez");
+		person2.setTelefono("75853-328");
+		person2.setEmail("peperez1985@gmail.com");
+		
+		List personas = Arrays.asList(person,person2);
+		
 		String mensaje = "hola desde el controlador";
 		modelo.addAttribute("saludo", mensaje);
+		//modelo.addAttribute("persona",person);
+		modelo.addAttribute("personas",personas);
 		
-		return "index.html";
+		return "index";
 	}
 
 }
---
+++ ProyectoSpring/src/main/java/com/Spring/model/Persona.java
@@ -0,0 +1,36 @@
+package com.Spring.model;
+
+public class Persona {
+	
+	private String Nombre;
+	private String Apellido;
+	private String Telefono;
+	private String Email;
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public String getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+
+}
